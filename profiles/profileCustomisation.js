ffcac68351c49fb1aa2681d047b02e7d717ef496
Paul Withers <pwithers@intec.co.uk>
2018-12-17 11:34:18 +0000

    Adapted from Customisation demo

---
+++ profiles/profileCustomisation.js
@@ -0,0 +1,15 @@
+////
+// @author Tony McGuckin, IBM
+// @name Profiles Customization
+// @version 0.1
+// @date February, 2017
+// 
+// Copied from samples
+//
+if(typeof(dojo) != "undefined") {
+    dojo.place(
+        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/customizer/samples/profiles/profilesCustomization.css?repoName=customizer\"></link>",
+        dojo.doc.head,
+        "last"
+    );
+}
